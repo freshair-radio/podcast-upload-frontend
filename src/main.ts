82848b6af19a72b8c2f4e4aac975ceae4e3c28cb
Samuel Macleod <samuel@macleod.space>
2019-08-21 14:23:12 +0100

    rm pwa

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
 new Vue({
-	router,
-	store,
-	data() {
-		return {
-			user: {},
-			auth: ""
-		};
-	},
-	render: h => h(App)
+  router,
+  store,
+  data() {
+    return {
+      user: {},
+      auth: ""
+    };
+  },
+  render: h => h(App)
 }).$mount("#app");
